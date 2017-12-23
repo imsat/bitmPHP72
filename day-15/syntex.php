fb07f0d55a3e79b8c1778854a9a58460420013d5
imsat <worksatyajit@gmail.com>
2017-12-23 17:18:23 +0600

    day15 commit4

---
+++ day-15/syntex.php
@@ -0,0 +1,72 @@
+<?php
+//echo "<html>";
+//    echo "<head>";
+//        echo "<title>";
+//            echo "PHP-15";
+//        echo "</title>";
+//    echo "</head>";
+//    echo "<body>";
+//        echo "<h1>This is Test PHP  </h1>";
+//        echo "<p>This is Test Paragraph</p>";
+//    echo "</body>";
+//echo "</html>";
+?>
+<!--<!doctype html>-->
+<!--<html lang="en">-->
+<!--<head>-->
+<!--    <meta charset="UTF-8">-->
+<!--    <meta name="viewport"-->
+<!--          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">-->
+<!--    <meta http-equiv="X-UA-Compatible" content="ie=edge">-->
+<!--    <title>home</title>-->
+<!--</head>-->
+<!--<body>-->
+<!--    <h1>--><?php //echo "This is Heading"; ?><!--</h1>-->
+<!--    <p>--><?php //echo "This is Paragraph"; ?><!--</p>-->
+<!--    <input type="text" value="--><?php //echo "hello world";?><!--">-->
+<!--</body>-->
+<!--</html>-->
+<?php
+##variable##
+// 3 major rules
+    /*
+     * Start With $;
+     * a-z, A-Z, 0-9
+     * No number in first
+     * */
+
+    $name;
+    $Name;
+    $Name;
+    $_6han;
+
+// 3 Standard rules
+/*
+ * small letter;
+ * Meaning full;
+ * Readable;
+ * */
+$name;
+$firstName = "Rakib"; //larabel Satandard
+$first_name = "Rakib"; //Satandard
+$name = 100;
+$number = 100;
+
+$firstName = "Rakib";
+
+$lasName = "Hasan";
+echo $firstName.' '.$lasName;
+
+//echo '$firstName';//string
+//echo "$firstName";//variable
+#type
+$namee = true;
+//operator
+//statement
+
+
+
+
+
+
+?>
