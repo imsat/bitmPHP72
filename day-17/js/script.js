87c27514b3c090427026de6422d48d7bb338ac59
imsat <worksatyajit@gmail.com>
2017-12-30 14:45:21 +0600

    day17 commit1

---
+++ day-17/js/script.js
@@ -0,0 +1,126 @@
+function checkFirstName() {
+    var firstNameValue = $('#firstName').val();
+    if(firstNameValue.length > 6 && firstNameValue.length <16){
+        $('#firstNameError').text(' ');
+        return true;
+    }else{
+        $('#firstNameError').text('First name should be 7 to 15 character');
+        return false;
+    }
+};
+
+function checkLastName() {
+    var lastNameValue = $('#lastName').val();
+    if(lastNameValue.length > 6 && lastNameValue.length <16){
+        $('#lastNameError').text(' ');
+        return true;
+    }else{
+        $('#lastNameError').text('Last name should be 7 to 15 character');
+        return false;
+    }
+};
+function checkEmailAddress() {
+    var emailAddressValue = $('#email').val();
+    var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-z0-9.-]+\.[a-zA-z]{3,3}$/i);
+    if(pattern.test(emailAddressValue)){
+        $('#emailError').text('');
+        return true;
+    }else{
+        $('#emailError').text('Envalid Email Address!!');
+        return false;
+    }
+};
+function checkPassword() {
+    var password = $('#password').val();
+    if(password.length > 2){
+        $('#passwordError').text(' ');
+        return true;
+    }else{
+        $('#passwordError').text('Password should be 3 Character');
+        return false;
+    }
+};
+
+function checkConfirmPassword() {
+    var password = $('#password').val();
+    var confirmPassword = $('#confirmPassword').val();
+    if(password == confirmPassword){
+        $('#confirmPasswordError').text(' ');
+        return true;
+    }else{
+        $('#confirmPasswordError').text('Password Not Matched!!');
+        return false;
+    }
+};
+
+$('#showPassword').click(function () {
+    if(this.checked){
+        $('#password').attr('type', 'text');
+        $('#confirmPassword').attr('type','text');
+    } else{
+        $('#password').attr('type', 'password');
+        $('#confirmPassword').attr('type','password');
+    }
+});
+
+function checkGender() {
+    var genderValue = $('input[type="radio"]:checked').val();
+    if(genderValue == "male"){
+        $('#genderError').text('');
+        return true;
+    }else if (genderValue == "female"){
+        $('#genderError').text('');
+        return true;
+    }else{
+    $('#genderError').text('Please select Gender info');
+}       return false;
+    };
+
+function checkDistrictName() {
+    var districtNameValue = $('#districtName').val();
+    if(districtNameValue == ' '){
+        $('#districtNameError').text('Please Select a District Name');
+        return false;
+    }else{
+        $('#districtNameError').text(' ');
+        return true;
+    }
+};
+
+
+
+$('#firstName').keyup(function () {
+    checkFirstName();
+});
+
+$('#lastName').keyup(function () {
+    checkLastName();
+});
+
+$('#email').keyup(function () {
+    checkEmailAddress();
+});
+
+$('#password').keyup(function () {
+    checkPassword();
+});
+
+$('#confirmPassword').keyup(function () {
+    checkConfirmPassword();
+});
+
+$('#districtName').change(function () {
+    checkDistrictName();
+})
+
+
+
+
+$('#testForm').submit(function () {
+    if(checkFirstName() == true && checkLastName() == true && checkEmailAddress() == true && checkConfirmPassword() == true && checkGender() == true && checkDistrictName() == true){
+        return true;
+    }else{
+        return false;
+    }
+
+});
---
+++ day-17/view.php
@@ -0,0 +1,45 @@
+<?php
+$firstName =$_POST['first_name'];
+$lastName =$_POST['last_name'];
+$fullName = $firstName.' '.$lastName;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Day-17</title>
+    <link rel="stylesheet" href="css/bootstrap.css" />
+</head>
+<body>
+<form action="view.php" method="post">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4">
+                <div class="form-group">
+                    <label for="firstName">First Name</label>
+                    <input type="text" placeholder="Your First Name"  name="first_name" id="firstName" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" name="last_name" placeholder="Your Last Name" id="lastName" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <label for="email">Full Name</label>
+                    <input type="text" placeholder="Full Name" name="full_name" id="full_name" value="<?php echo $fullName;?>" class="form-control"/>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-outline-primary btn-block" id="btn">Submit</button>
+                </div>
+            </div>
+            <div class="col-sm-4"></div>
+        </div>
+    </div>
+</form>
+<script src="js/bootstrap.js"></script>
+<script src="js/jquery-3.2.1.js"></script>
+<!--<script src="js/script.js"></script>-->
+</body>
+</html>
